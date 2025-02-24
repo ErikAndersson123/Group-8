5b4771423ca40f9b98b57a8dc6bcfb00c57a76c8
Susuwi <72568790+Susuwi@users.noreply.github.com>
2025-02-24 17:44:39 +0100

    just a start

--- client/ChatroomView.java
+++ client/ChatroomView.java
@@ -1,17 +1,35 @@
 package client;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.*;
 
-public class ChatroomView extends JFrame implements View{
+import server.User;
 
-    
-    public ChatroomView() {
-        
-        setSize(800,600);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-        
-        
-        
-    }
+public class ChatroomView extends JFrame implements View{
+	public static int RoomID;
+	public static User u;
+	public ChatroomView(User user) {
+		u = user;
+		RoomID = 1;
+		setPreferredSize(new Dimension(800,600));
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		JPanel pa = new JPanel();
+		pa.setSize(200,600);
+		pa.setBackground(Color.LIGHT_GRAY);
+		
+		PanelView pv = new PanelView(600,600);
+		
+		add(pa);
+		add(pv);
+		System.out.println(pv.getLocation());
+		pack();
+		setVisible(true);
+		
+		
+		
+	}
+	
+	
 }
--- client/UserLoginView.java
+++ client/UserLoginView.java
@@ -9,126 +9,129 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.*;
-
 import server.User;
 import server.PortalConnection;
 import java.sql.*;
 
-public class UserLoginView extends JFrame implements View {
-    private String pswrd = "";
-    public static void main(String[] args) {
-        UserLoginView d = new UserLoginView();
-        
-        
-    }
-    @SuppressWarnings("deprecation")
-    private UserLoginView() {
-        setSize(500, 400);
-        setLayout(null);
-        JLabel l1 = new JLabel("Enter Username:");
-        l1.setBounds(40, 50, 200, 30);
-        JTextField tx1 = new JTextField("");
-        tx1.setBounds(40, 80, 200, 30);
-        tx1.addActionListener(e->{
-            System.out.println(tx1.getText());
-        });
-        JLabel l2 = new JLabel("Enter Password:");
-        l2.setBounds(40, 110, 200, 30);
-        JTextField tx2 = new JTextField("");
-        tx2.setBounds(40, 140, 200, 30);
-        tx2.addFocusListener(new FocusListener(){
+public class UserLoginView extends JFrame implements View{
+	private String pswrd = "";
+	public static void main(String[] args) {
+		UserLoginView d = new UserLoginView();
+		
+		
+	}
+	@SuppressWarnings("deprecation")
+	private UserLoginView(){
+		setSize(500, 400);
+		setLayout(null);
+		JLabel l1 = new JLabel("Enter Username:");
+		l1.setBounds(40, 50, 200, 30);
+		JTextField tx1 = new JTextField("");
+		tx1.setBounds(40, 80, 200, 30);
+		tx1.addActionListener(e->{
+			System.out.println(tx1.getText());
+		});
+		JLabel l2 = new JLabel("Enter Password:");
+		l2.setBounds(40, 110, 200, 30);
+		JTextField tx2 = new JTextField("");
+		tx2.setBounds(40, 140, 200, 30);
+		tx2.addFocusListener(new FocusListener(){
 
-            @Override
-            public void focusGained(FocusEvent e) {
-                tx2.setText(pswrd);
-                
-            }
+			@Override
+			public void focusGained(FocusEvent e) {
+				tx2.setText(pswrd);
+				
+			}
 
-            @Override
-            public void focusLost(FocusEvent e) {
-                pswrd = tx2.getText();
-                tx2.setText("");
-                for (int i = 0; i < pswrd.length(); i++) {
-                    tx2.setText("*"+tx2.getText());
-                }
-            }
-            
-        });
-    
-        JButton b1 = new JButton("Login");
-        b1.setBounds(160, 180, 80, 30);
-        b1.addActionListener(e->{
-            System.out.println(tx2.getText());
-            System.out.println(tx1.getText());
-            String username = tx1.getText();
-            String password = tx2.getText();
+			@Override
+			public void focusLost(FocusEvent e) {
+				pswrd = tx2.getText();
+				tx2.setText("");
+				for (int i = 0; i < pswrd.length(); i++) {
+					tx2.setText("*"+tx2.getText());
+				}
+			}
+			
+		});
+	
+		JButton b1 = new JButton("Login");
+		b1.setBounds(160, 180, 80, 30);
+		b1.addActionListener(e->{
+			if(pswrd.length() < 1)
+				pswrd = tx2.getText();
+			System.out.println(pswrd);
+			System.out.println(tx1.getText());
+			String username = tx1.getText();
+            String password = pswrd;
             PortalConnection p = new PortalConnection();
             User user = new User(username, password);
-            if (p.authenticateUser(user)) {
-                new ChatroomView();
-                this.dispose();
-            }
-        });
-        b1.setCursor(new Cursor(HAND_CURSOR));
-        b1.setFocusable(false);
-        add(b1);
-        add(tx1);
-        add(l1);
-        add(tx2);
-        add(l2);
-        addMouseListener(new MouseListener() {
+				if (p.authenticateUser(user)) {
+		               new ChatroomView(user);
+		               this.dispose();
+		            }
             
-            
-        
+			
+		});
+		b1.setCursor(new Cursor(HAND_CURSOR));
+		b1.setFocusable(false);
+		add(b1);
+		add(tx1);
+		add(l1);
+		add(tx2);
+		add(l2);
+		addMouseListener(new MouseListener() {
+			
+			
+		
 
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                // TODO Auto-generated method stub
-                if(isIn(e.getPoint(),tx2.getLocation(),tx2.getSize())) {
-                    tx2.requestFocus();
-                    System.out.println("d");
-                }
-                else if(isIn(e.getPoint(),tx1.getLocation(),tx1.getSize())) {
-                    tx1.requestFocus();
-                
-                }    else
-                    requestFocus();
-                    
-                
-            }
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				if(isIn(e.getPoint(),tx2.getLocation(),tx2.getSize())) {
+					tx2.requestFocus();
+					System.out.println("d");
+				}
+				else if(isIn(e.getPoint(),tx1.getLocation(),tx1.getSize())) {
+					tx1.requestFocus();
+				
+				}	else
+					requestFocus();
+					
+				
+			}
 
-            @Override
-            public void mousePressed(MouseEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
 
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
 
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
 
-            @Override
-            public void mouseExited(MouseEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
-        });
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-    }
-    protected boolean isIn(Point point, Point location, Dimension size) {
-        System.out.println(point);
-        if(point.x > location.x && point.x < location.x + size.width)
-            if (point.y > location.y && point.y < location.y + size.height)
-                return true;
-        return false;
-    }
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	protected boolean isIn(Point point, Point location, Dimension size) {
+		System.out.println(point);
+		if(point.x > location.x && point.x < location.x + size.width)
+			if (point.y > location.y && point.y < location.y + size.height)
+				return true;
+		return false;
+	}
 }
