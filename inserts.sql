cf6dff2dc714be18f113c8e688bc59c3de529e8a
ErikAndersson123 <eriuv@chalmers.se>
2025-03-02 14:00:35 +0100

    Add files via upload

---
+++ inserts.sql
@@ -0,0 +1,29 @@
+INSERT INTO Users VALUES('Erik','123');
+INSERT INTO Users VALUES('William','123');
+INSERT INTO Users VALUES('Alexander','123');
+INSERT INTO Users VALUES('Anton','123');
+INSERT INTO Users VALUES('Gabriel','123');
+
+INSERT INTO Chatrooms VALUES(1, 'Chat1');
+INSERT INTO Chatrooms VALUES(2, 'Chat2');
+INSERT INTO Chatrooms VALUES(3, 'Chat3');
+INSERT INTO Chatrooms VALUES(4, 'Chat4');
+INSERT INTO Chatrooms VALUES(5, 'Chat5');
+
+INSERT INTO ChatroomUsers VALUES(1, 1);
+INSERT INTO ChatroomUsers VALUES(1, 2);
+INSERT INTO ChatroomUsers VALUES(1, 3);
+INSERT INTO ChatroomUsers VALUES(1, 4);
+INSERT INTO ChatroomUsers VALUES(1, 5);
+
+INSERT INTO ChatroomUsers VALUES(2, 1);
+INSERT INTO ChatroomUsers VALUES(2, 2);
+INSERT INTO ChatroomUsers VALUES(2, 3);
+INSERT INTO ChatroomUsers VALUES(2, 4);
+INSERT INTO ChatroomUsers VALUES(2, 5);
+
+INSERT INTO ChatroomUsers VALUES(3, 1);
+INSERT INTO ChatroomUsers VALUES(3, 2);
+INSERT INTO ChatroomUsers VALUES(3, 3);
+INSERT INTO ChatroomUsers VALUES(3, 4);
+INSERT INTO ChatroomUsers VALUES(3, 5);
