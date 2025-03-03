810c70f74c90d20d6efdab81c908cb5efc26c943
Carpeen <carpenfelt@gmail.com>
2025-03-03 02:43:24 +0100

    Add files via upload

---
+++ sqlserver/Test.sql
@@ -0,0 +1,6 @@
+SELECT * FROM Users;
+SELECT * FROM Chatrooms;
+SELECT * FROM Messages;
+SELECT * FROM ChatroomUsers;
+SELECT * FROM Messages WHERE roomID = 1;
+SELECT messageID, image FROM Messages WHERE messageID = 1;
