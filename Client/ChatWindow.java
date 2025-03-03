43d5d782f73119653dd2f5dbbb75875f7dc65d6e
Carpeen <carpenfelt@gmail.com>
2025-03-03 03:18:26 +0100

    Add files via upload

--- Client/ChatWindow.java
+++ Client/ChatWindow.java
@@ -6,14 +6,18 @@ import Server.Message;
 import Server.User;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.LinkedList;
 
 public class ChatWindow extends JPanel {
     private static final long serialVersionUID = 1L;
     LinkedList<User> Users;
-
+    RMIClient rmiClient;
     public ChatWindow(LinkedList<Message> messages, RMIClient c) throws Exception {
-         //RMIClient rmiClient = c;
+        this.rmiClient = c;
          Users = c.getClientLogic().getUsers();
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)); // Vertical stacking
 
@@ -45,7 +49,7 @@ public class ChatWindow extends JPanel {
         messagePanel.setMaximumSize(new Dimension(565, 80));
         messagePanel.setMinimumSize(new Dimension(565, 80));
 
-        
+
 
         String username = "Unknown";
         for (User user : Users) {
@@ -54,7 +58,7 @@ public class ChatWindow extends JPanel {
                 break;
             }
         }
-        
+    
         JLabel senderLabel = new JLabel(username + ":");
         JTextArea textLabel = new JTextArea(msg.getText());
         textLabel.setEditable(false);
@@ -63,7 +67,47 @@ public class ChatWindow extends JPanel {
         
         messagePanel.add(senderLabel, BorderLayout.NORTH);
         messagePanel.add(textLabel, BorderLayout.CENTER);
+        if (msg.getImage() != null) {
+            JButton LoadImageButton = new JButton("Load Image");
+            LoadImageButton.setPreferredSize(new Dimension(120, 40));
+
+            LoadImageButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        byte[] imageData = rmiClient.getClientLogic().getImageFile(msg.getMessageID()); 
+                        if (imageData != null) {
+                            String outputPath = "received_image_" + msg.getMessageID() + ".jpg";
+                            try (FileOutputStream fos = new FileOutputStream(outputPath)) {
+                                fos.write(imageData);
+                            }
+                            System.out.println("Image received and saved as: " + outputPath);
+                            showImage(outputPath);
+                        } else {
+                            System.out.println("No image found!");
+                        }
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            });
+
+            messagePanel.add(LoadImageButton, BorderLayout.EAST);
+        }
+
+        
 
         return messagePanel;
+    }
+
+    public static void showImage(String imagePath) {
+        JFrame frame = new JFrame("Received Image");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+
+        ImageIcon imageIcon = new ImageIcon(imagePath);
+        JLabel label = new JLabel(imageIcon);
+
+        frame.add(label, BorderLayout.CENTER);
+        frame.setVisible(true);
     }
 }
