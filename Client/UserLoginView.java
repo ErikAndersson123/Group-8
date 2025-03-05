922e1702a0d44146d882b0ec613a3b76e1c2bf03
Susuwi <72568790+Susuwi@users.noreply.github.com>
2025-03-05 18:00:35 +0100

    Added register user functionalities

--- Client/UserLoginView.java
+++ Client/UserLoginView.java
@@ -3,26 +3,24 @@ package Client;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Point;
-
-
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
-
 import java.rmi.RemoteException;
 
 import javax.swing.*;
 import Server.User;
 
-
-
-
 public class UserLoginView extends JFrame {
 
     
-    private String pswrd = "";
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String pswrd = "";
     private static RMIClient rmiClient;
-    char ogEcho;
+    private char ogEcho;
+    private boolean login = true;
     public static void main(String[] args) throws RemoteException {
         rmiClient = new RMIClient();
         new UserLoginView();
@@ -36,12 +34,9 @@ public class UserLoginView extends JFrame {
         l1.setBounds(40, 50, 200, 30);
         JTextField tx1 = new JTextField("");
         tx1.setBounds(40, 80, 200, 30);
-        tx1.addActionListener(e->{
-            System.out.println(tx1.getText());
-        });
         JLabel l2 = new JLabel("Enter Password:");
         l2.setBounds(40, 110, 200, 30);
-        JPasswordField k = new JPasswordField();
+        JPasswordField k = new JPasswordField("");
         k.setBounds(40, 140, 200, 30);
         ogEcho = k.getEchoChar();
         JButton b1 = new JButton("Login");
@@ -53,6 +48,8 @@ public class UserLoginView extends JFrame {
             String username = tx1.getText();
             String password = pswrd;
             User user = new User(username, password);
+            if (login) {
+            
             try {
                 user.setUserID(rmiClient.getClientLogic().getUserID(user));
             } catch (Exception e1) {
@@ -65,19 +62,46 @@ public class UserLoginView extends JFrame {
                 this.dispose();
                 }
             } catch (Exception e1) {
-                        //System.out.println("oj");
+                
                 e1.printStackTrace();
             }
             
-            
+            } else {
+            	try {
+					rmiClient.getClientLogic().createUser(user);
+					setTitle("User succesfully Created");
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+            }
         });
         b1.setCursor(new Cursor(HAND_CURSOR));
         b1.setFocusable(false);
+        JButton b2 = new JButton("Sign up");
+        b2.setBounds(40, 180, 80, 30);
+        b2.setFocusable(false);
+        b2.addActionListener(e->{
+        	if (login) {
+        		login = false;
+        		l1.setText("Set Username:");
+        		l2.setText("Set Password:");
+        		b1.setText("Create");
+        		b2.setText("Back");
+        	} else {
+        		login = true;
+        		l1.setText("Enter Username:");
+        		l2.setText("Enter Password:");
+        		b1.setText("Login");
+        		b2.setText("Sign up");
+        	}
+        });
         add(b1);
         add(tx1);
         add(l1);
         add(l2);
         add(k);
+        add(b2);
         k.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {}
