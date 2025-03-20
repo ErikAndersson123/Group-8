3eaa684a4c980b87871a250174e07d95cb85708b
ErikAndersson123 <eriuv@chalmers.se>
2025-03-20 18:06:14 +0100

    Add files via upload

--- Server/DataLogic.java
+++ Server/DataLogic.java
@@ -13,362 +13,107 @@ import java.util.*;
 
 public class DataLogic extends UnicastRemoteObject implements Subject {
     
-    private DatabaseHandler dh = new DatabaseHandler();
+    private DatabaseHandler databaseHandler = new DatabaseHandler();
     
     private static final long serialVersionUID = 1L;
     private List<Observer> observers = new ArrayList<>();
-    private LinkedList<User> users = new LinkedList<>();
-    private LinkedList<Chatroom> chatrooms = new LinkedList<>();
+    
+    private UserHandler userHandler = new UserHandler(databaseHandler);
+    private ChatroomHandler chatroomHandler = new ChatroomHandler(databaseHandler);
+    private MessageHandler messageHandler = new MessageHandler(databaseHandler);
     
     public DataLogic() throws RemoteException {
         super();
-        loadUsersFromDatabase();
-        loadChatroomsFromDatabase();
-        loadMessagesFromDatabase();
-        loadChatroomUsersFromDatabase();
     }
     
     public void createUser(User user) throws RemoteException {
-        dh.registerUser(user);
-        user.setUserID(dh.getUserID(user));
-        users.add(user);
-        System.out.println("User created");
+        userHandler.createUser(user);
         notifySubscribers();
     }
     
     public void deleteUser(User user) throws RemoteException {
-        dh.unregisterUser(user);
-        users.remove(user);
-        for (Chatroom chatroom : chatrooms) {
-            chatroom.removeChatroomUser(user);
-        }
-        System.out.println("User deleted");
+        userHandler.deleteUser(user);
+        chatroomHandler.deleteUserFromChatrooms(user);
         notifySubscribers();
     }
 
     public boolean authenticateUser(User user) throws RemoteException {
-        for (User u : users) {
-            if (u.getUsername().equals(user.getUsername()) && u.getPassword().equals(user.getPassword())) {
-                System.out.println("User authorized");
-                return true;
-            }
-        }
-        System.out.println("User not authorized");
-        return false;
+        return userHandler.authenticateUser(user);
     }
 
     public void createChatroom(Chatroom chatroom) throws RemoteException {
-        chatroom.setRoomID(dh.nextAvailableRoomID());
-        dh.registerChatroom(chatroom);
-        chatrooms.add(chatroom);
-        System.out.println("Chatroom created");
+        chatroomHandler.createChatroom(chatroom);
         notifySubscribers();
     }
 
     public void deleteChatroom(Chatroom chatroom) throws RemoteException {
-        dh.unregisterChatroom(chatroom);
-        chatrooms.remove(chatroom);
-        System.out.println("Chatroom deleted");
+        chatroomHandler.deleteChatroom(chatroom);
         notifySubscribers();
     }
 
     public void createMessage(Message message) throws RemoteException {
-        for (Chatroom c : chatrooms) {
-            if (c.getRoomID() == message.getRoomID()) {
-                message.setMessageID(dh.nextAvailableMessageID(c));
-                dh.addMessage(message);
-                c.addMessage(message);
-                
-            }
-        }
-        System.out.println("Message created");
+        chatroomHandler.createMessage(message);
         notifySubscribers();
     }
 
     public void deleteMessage(Message message) throws RemoteException {
-        for (Chatroom c : chatrooms) {
-            if (c.getRoomID() == message.getRoomID()) {
-                dh.removeMessage(message);
-                c.removeMessage(message);
-                
-            }
-        }
-        System.out.println("Message deleted");
+        chatroomHandler.deleteMessage(message);
         notifySubscribers();
     }
 
     public void addChatroomUser(User user, Chatroom chatroom) throws RemoteException {
-        dh.registerChatroomUser(user, chatroom);
-        for (Chatroom c : chatrooms) {
-            if (c.getRoomID() == chatroom.getRoomID()) {
-                
-                c.setChatroomUsers(dh.getAllChatroomUsers(chatroom));
-            }
-        }
-        System.out.println("User added to chatroom");
+        chatroomHandler.addChatroomUser(user, chatroom);
         notifySubscribers();
     }
 
     public void removeChatroomUser(User user, Chatroom chatroom) throws RemoteException {
-        dh.unregisterChatroomUser(user, chatroom);
-        for (Chatroom c : chatrooms) {
-            if (c.getRoomID() == chatroom.getRoomID()) {
-                
-                
-                c.setChatroomUsers(dh.getAllChatroomUsers(chatroom));
-            }
-        }
-        System.out.println("User removed from chatroom");
+        chatroomHandler.removeChatroomUser(user, chatroom);
         notifySubscribers();
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    public byte[] getImageFile(Message msg) throws RemoteException {
-
-        String imageFilename = dh.getImagePath(msg);
-        System.out.println(imageFilename);
-
-        if (imageFilename == null) {
-            System.out.println("Image path is NULL for message ID: " + msg.getMessageID()+ " in roomId: " + msg.getRoomID());
-            return null;
-        }
-
-        String baseDir = "C:\\Users\\Per\\Desktop\\Labb Objektorienterade Applikationer\\Labb Objektorienterade Applikationer v.17\\Labb Objektorienterade Applikationer v.17\\"; //Change this
-
-        String imagePath = baseDir +imageFilename;
-
-        System.out.println(imagePath);
-    
-        File file = new File(imagePath);
-        if (!file.exists()) {
-            System.out.println("Image file not found at: " + imagePath);
-            return null;
-        }
-    
-        try (FileInputStream fis = new FileInputStream(file)) {
-            byte[] fileData = new byte[(int) file.length()];
-            fis.read(fileData);
-            return fileData;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+    public byte[] getImageFile(Message message) throws RemoteException {
+        return messageHandler.getImageFile(message);
     }
     
     public void uploadImage(File image){
-        String imageName = image.getName();
-        String outputPath = imageName;
-
-        try (FileInputStream fis = new FileInputStream(image);
-             FileOutputStream fos = new FileOutputStream(outputPath)) {
-
-            byte[] imageData = new byte[(int) image.length()];
-            fis.read(imageData);
-            fos.write(imageData);
-
-            System.out.println("Image received and saved as: " + outputPath);
-
-        } catch (IOException ex) {
-            System.out.println("Error processing image: " + ex.getMessage());
-            ex.printStackTrace();
-        }
+        messageHandler.uploadImage(image);
     }
-
-    
-    
-    
-    /*
-
-    public void uploadImage(File image) {
-        String imageName = image.getName();
-        String outputPath = imageName;
-
-        try (FileInputStream fis = new FileInputStream(image);
-             FileOutputStream fos = new FileOutputStream(outputPath)) {
-
-            byte[] buffer = new byte[4096]; // Use a buffer to avoid memory issues
-            int bytesRead;
-
-            while ((bytesRead = fis.read(buffer)) != -1) {
-                fos.write(buffer, 0, bytesRead);
-            }
-
-            System.out.println("Image received and saved as: " + outputPath);
-
-        } catch (IOException ex) {
-            System.out.println("Error processing image: " + ex.getMessage());
-            ex.printStackTrace();
-        }
-    }
-
-    
-    
-    
-    
-
-
-    public void uploadImage(File image){
-        String imageName = image.getName();
-        String outputPath = imageName;
-
-        try (FileInputStream fis = new FileInputStream(image);
-             FileOutputStream fos = new FileOutputStream(outputPath)) {
-
-            byte[] imageData = new byte[(int) image.length()];
-            fis.read(imageData);  
-
-            System.out.println("Image received and saved as: " + outputPath);
-            
-        } catch (IOException ex) {
-            System.out.println("Error processing image: " + ex.getMessage());
-            ex.printStackTrace();
-        }
-    }
-    
-    */
-    
-    
-    
-    
-
-
 
     public LinkedList<Message> chatHistory(Chatroom chatroom) throws RemoteException{
-        for (Chatroom c : chatrooms) {
-            if (c.getRoomID() == chatroom.getRoomID()) {
-                System.out.println("Chat history sent");
-                return c.getChatHistory();
-            }
-        }
-        System.out.println("Chat history sent");
-        return null;
+        return chatroomHandler.chatHistory(chatroom);
     }
     
     public String messageInfo(Message message) throws RemoteException{
-
-        
-        for (User u : users) {
-            if (u.getUserID() == message.getSenderID()) {
-                return u.getUsername();
-            }
-        }
-        return "Deleted User";
+        return userHandler.messageInfo(message);
     }
     
     public LinkedList<User> chatroomUsers(Chatroom chatroom) throws RemoteException{
-        for (Chatroom c : chatrooms) {
-            if (c.getRoomID() == chatroom.getRoomID()) {
-                System.out.println("Chatroom Users sent");
-                return c.getChatroomUsers();
-            }
-        }
-        System.out.println("Chatroom Users sent");
-        return null;
+        return chatroomHandler.chatroomUsers(chatroom);
     }
      
     public LinkedList<Chatroom> userChatrooms(User user) throws RemoteException {
-        LinkedList<Chatroom> list = new LinkedList<>();
-        for (Chatroom c: chatrooms) {
-            for (User u : c.getChatroomUsers()) {
-                if (u.getUserID() == user.getUserID()) {
-                    list.add(c);
-                }
-            }
-        }
-        System.out.println("User chatrooms sent");
-        return list;
+        return chatroomHandler.userChatrooms(user);
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    public void loadUsersFromDatabase() {
-        users.clear();
-        users.addAll(dh.getAllUsers());
-    }
-    
-    public void loadChatroomsFromDatabase() {
-        chatrooms.clear();
-        chatrooms.addAll(dh.getAllChatrooms());
-    }
-    
-    public void loadMessagesFromDatabase() {
-            for (Chatroom chatroom : chatrooms) {
-            LinkedList<Message> messages = dh.getAllMessages(chatroom);
-            chatroom.setChatHistory(messages);
-        }
-    }
-    
-    public void loadChatroomUsersFromDatabase() {
-        for (Chatroom chatroom : chatrooms) {
-            LinkedList<User> chatroomUsers = dh.getAllChatroomUsers(chatroom);
-            chatroom.setChatroomUsers(chatroomUsers);
-        }
-    }
-
-
-    
-    
-    
     
     public int getUserID(User user) throws RemoteException {
-        return dh.getUserID(user);
+        return databaseHandler.getUserID(user);
     }
     
     public int getRoomID(Chatroom chatroom) throws RemoteException {
-        return dh.getRoomID(chatroom);
+        return databaseHandler.getRoomID(chatroom);
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    public boolean inChatroom(User user, Chatroom chatroom) throws RemoteException {        
-        for (Chatroom c : userChatrooms(user)) {
-            if (c.getRoomID() == chatroom.getRoomID()) {
-                return true;
-            }
-        }
-        return false;
+    public boolean inChatroom(User user, Chatroom chatroom) throws RemoteException {
+        return chatroomHandler.inChatroom(user, chatroom);
     }
-    
-    
-    
-    
     
     public LinkedList<User> getUsers() {
-        return this.users;
+        return databaseHandler.getAllUsers();
     }
     
     public LinkedList<Chatroom> getChatrooms() {
-        return this.chatrooms;
+        return databaseHandler.getAllChatrooms();
     }
-    
-    
-    
-    
-    
-    
     
     public void addSubscriber(Observer observer) throws RemoteException {
         observers.add(observer);
@@ -379,7 +124,6 @@ public class DataLogic extends UnicastRemoteObject implements Subject {
     }
     
     public void notifySubscribers() throws RemoteException {
-        System.out.println("Hej");
         for (Observer observer : observers) {
             try {
                 observer.update();
